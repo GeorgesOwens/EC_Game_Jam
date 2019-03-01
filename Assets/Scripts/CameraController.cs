d4862eb9426981979df783822f8d49daf4d0b16f
Alcarkse <15620365+Alcarkse@users.noreply.github.com>
2019-03-01 19:01:39 +0000

    Vertical Prototype

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Transform player;
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3
+        (
+            transform.position.x,
+            player.position.y,
+            transform.position.z
+        );
+    }
+}
