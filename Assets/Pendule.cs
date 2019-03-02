b8b6fd69d722a7d9c8593f8275ec531cd15ada07
Alcarkse <15620365+Alcarkse@users.noreply.github.com>
2019-03-02 14:42:32 +0000

    Added Pendulum

--- Assets/Pendule.cs
+++ Assets/Pendule.cs
@@ -8,6 +8,8 @@ public class Pendule : MonoBehaviour
     [Header("Animation Settings")]
     [Range(0, 5)]
     public float speed = 1;
+    [Range(0,1)]
+    public float amplitude = 0.33f;
 
     private Animator animController;
 
@@ -19,5 +21,7 @@ public class Pendule : MonoBehaviour
     private void Update()
     {
         animController.speed = speed;
+
+        animController.SetFloat("SwingAmplitude", amplitude);
     }
 }
